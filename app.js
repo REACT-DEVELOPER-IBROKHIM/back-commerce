a3b684ed37f23df0b7da5e973bd81e3c2455e5fd
REACT-DEVELOPER-IBROKHIM <ij.jalalov.ibrokhim@gmail.com>
2024-07-11 22:47:08 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
+
+app.use(cors());
+app.options('*', cors())
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use(errorHandler);
+
+//Routes
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+app.use(`/welcome`, (req,res,next)=>{
+    res.status(200).json({
+        success:'success'
+    })
+});
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    dbName : 'ecommerce-database'
+})
+.then(()=>{
+    console.log('we connected mongo')
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.listen(9000, ()=>{
+    console.log("server is running");
+})
+//http://localhost:3000/public/uploads/macbook.png
